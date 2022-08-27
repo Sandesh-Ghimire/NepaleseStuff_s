0a4be1a69acb64721eeaccfb0555bfbbc26235e2
nirajgiriXD <nirajgiri9860@gmail.com>
2022-08-27 17:52:28 +0530

    dherai kuro update gareko xu

--- js/active.js
+++ js/active.js
@@ -108,7 +108,7 @@
     // :: 6.0 ScrollUp Active Code
     if ($.fn.scrollUp) {
         browserWindow.scrollUp({
-            scrollSpeed: 1500,
+            scrollSpeed: 1000,
             scrollText: '<i class="fa fa-angle-up"></i>'
         });
     }
