95db53faed7bd96ab9195200ceb2c506f2b8cfd2
sandesh-ghimire <ghimiresandesh45@gmail.com>
2022-09-01 18:49:18 +0530

    made changes to top nav-bar and added about page with responsive design.

--- about.php
+++ about.php
@@ -12,7 +12,6 @@
     <title>Nepalese Stuff</title>
 
     <!-- Bootstrap -->
-    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Font Awesome -->
@@ -23,11 +22,29 @@
 
     <!-- Stylesheet -->
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="about.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="    https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+
+    <link rel="stylesheet" href=" https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
 </head>
 
+
+
+
+
+
+
+
 <body>
-    <!-- Preloader -->
+
+
     <div class="preloader d-flex align-items-center justify-content-center">
         <div class="lds-ellipsis">
             <div></div>
@@ -44,13 +61,7 @@
             <li>|</li>
             <li><a onclick="getDynamicContent('about.php')" class="setCursorPointer">About</a></li>
             <li>|</li>
-            <!-- <li><a href="about.php">About</a>
-            </li>
-            <li>|</li> -->
-
             <li><a onclick="getDynamicContent('contact.php')" class="setCursorPointer">Contact</a></li>
-           
-
             <li>|</li>
             <li><a onclick="getDynamicContent('faq.php')" class="setCursorPointer">FAQ</a></li>
         </ul>
@@ -301,80 +312,126 @@
     </header>
     <!-- ##### Header Area End ##### -->
 
-    <!-- ##### Dynamic Content Allocation ##### -->
-    <div id="dynamic-content"></div>
 
-    <!-- ##### Footer Area Start ##### -->
-    <footer>
-        <div class="row">
-            <div class="col-3 ftr-col-3">
-                <div class="link-cat" onclick="footerToggle(this)">
-                    <span class="footer-toggle"></span>
-                    <span class="footer-cat">Nepalese Stuff</span>
-                </div>
-                <ul class="footer-cat-links" style="width: 80%; pointer-events: none;">
-                    <li><br></li>
-                    <li>
-                        <a href="#" style="text-align: justify;">
-                            Nepalese Stuff is a blog website dedicated for easy access of reliable information about Nepal.
-                        </a>
-                    </li>
-                    <li><br></li>
-                </ul>
-            </div>
+<div class="bg-light">
+  <div class="container py-5">
+    <div class="row h-100 align-items-center py-5">
+      <div class="col-lg-6">
+        <h1 class="display-4">About us page</h1>
+        <p class="lead text-muted mb-0">Create a minimal about us page using Bootstrap 4.</p>
+        <p class="lead text-muted">Snippet by <a href="https://bootstrapious.com/snippets" class="text-muted"> 
+                    <u>Bootstrapious</u></a>
+        </p>
+      </div>
+      <div class="col-lg-6 d-none d-lg-block"><img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid"></div>
+    </div>
+  </div>
+</div>
 
-            <div class="col-3 ftr-col-3">
-                <div class="link-cat" onclick="footerToggle(this)">
-                    <span class="footer-toggle"></span>
-                    <span class="footer-cat">Services</span>
-                </div>
-                <ul class="footer-cat-links" style="cursor: context-menu;">
-                    <li><br></li>
-                    <li><a name=""><span>Service1</span></a></li>
-                    <li><a name=""><span>Service2</span></a></li>
-                    <li><a name=""><span>Service3</span></a></li>
-                    <li><a name=""><span>Service4</span></a></li>
-                    <li><br></li>
-                </ul>
-            </div>
+<div class="bg-white py-5">
+  <div class="container py-5">
+    <div class="row align-items-center mb-5">
+      <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+        <h2 class="font-weight-light">Lorem ipsum dolor sit amet</h2>
+        <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+      </div>
+      <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+    </div>
+    <div class="row align-items-center">
+      <div class="col-lg-5 px-5 mx-auto"><img src="https://bootstrapious.com/i/snippets/sn-about/img-2.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+      <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
+        <h2 class="font-weight-light">Lorem ipsum dolor sit amet</h2>
+        <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+      </div>
+    </div>
+  </div>
+</div>
 
-            <div class="col-3 ftr-col-3">
-                <div class="link-cat" onclick="footerToggle(this)">
-                    <span class="footer-toggle"></span>
-                    <span class="footer-cat">Quick Links</span>
-                </div>
-                <ul class="footer-cat-links">
-                    <li><br></li>
-                    <li><a href="index.php"><span>Home</span></a></li>
-                    <li><a href="about.php"><span>About</span></a></li>
-                    <li><a href="contact.php"><span>Contact</span></a></li>
-                    <li><a href="faq.php"><span>FAQ</span></a></li>
-                    <li><br></li>
-                </ul>
-            </div>
+<div class="bg-light py-5">
+  <div class="container py-5">
+    <div class="row mb-4">
+      <div class="col-lg-5">
+        <h2 class="display-4 font-weight-light">Our team</h2>
+        <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </div>
 
-            <div class="col-3 ftr-col-3">
-                <div class="link-cat" onclick="footerToggle(this)">
-                    <span class="footer-toggle"></span>
-                    <span class="footer-cat">Contact Us</span>
-                </div>
-                <ul class="footer-cat-links">
-                    <li><br></li>
-                    <li><i class="fa fa-phone"></i><a name="">980-0000-0000</a></li>
-                    <li><i class="fa fa-facebook"></i><a href="https://www.facebook.com/epsnepal4" target="_blank">nepalesestuff</a></li>
-                    <li><i class="fa fa-map-marker"></i><a name="">Kathmandu, Nepal</a></li>
-                    <li><br></li>
-                </ul>
-            </div>
+    <div class="row text-center">
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Manuella Nevoresky</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
         </div>
-        <div id="copyright">
-            Copyright &copy; Nepalese Stuff
-            <script>
-                document.write(new Date().getFullYear())
-            </script>
+      </div>
+      <!-- End-->
+
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
         </div>
-    </footer>
-    <!-- ##### Footer Area Start ##### -->
+      </div>
+      <!-- End-->
+
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- End-->
+
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- End-->
+
+    </div>
+  </div>
+</div>
+
+
+<footer class="bg-light pb-5">
+  <div class="container text-center">
+    <p class="font-italic text-muted mb-0">&copy; Copyrights Company.com All rights reserved.</p>
+
+  </div>
+
+
+
+
+  
+</footer>
+
+
+
 
     <!-- ##### All Javascript Script ##### -->
     <!-- jQuery-2.2.4 js -->
@@ -419,6 +476,7 @@
             $('#dynamic-content').load('error.php')
         }
     </script>
+
 
 </body>
 
