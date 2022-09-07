fbb7171ba7c3aa09f48b29e2895e15aa1ed245ea
nirajgirixd <nirajgiri9860@gmail.com>
2022-09-07 17:15:21 +0530

    updated about.php

--- about.php
+++ about.php
@@ -2,37 +2,54 @@
 
 <div style="margin-top: -21px;">
     <div class="bg-light">
-    <div class="container py-5">
-        <div class="row h-100 align-items-center py-5">
-        <div class="col-lg-6">
-            <h1 class="display-4">Our Mission</h1>
-            <p class="lead text-muted mb-0">We are providing easy navigation,relaible,updated information to users of all kind who seeks the information about our country Nepal.</p>
-            <p class="lead text-muted">Snippet by <a href="https://bootstrapious.com/snippets" class="text-muted"> 
-                        <u>Learn more</u></a>
-            </p>
+        <div class="container py-5">
+            <div class="row h-100 align-items-center py-10">
+                <div class="col-lg-6">
+                    <h1 class="display-4">Our Mission</h1>
+                    <p class="font-italic text-muted mb-0">We are providing easy navigation,relaible,updated information to users of all kind who seeks the information about our country Nepal.</p>
+                </div>
+                <div class="col-lg-6 d-none d-lg-block">
+                    <img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid">
+                </div>
+            </div>
         </div>
-        <div class="col-lg-6 d-none d-lg-block"><img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid"></div>
-        </div>
-    </div>
     </div>
 
     <div class="bg-white py-5">
-    <div class="container py-5">
-        <div class="row align-items-center mb-5">
-        <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-            <h2 class="font-weight-light">Our story</h2>
-            <p class="font-italic text-muted mb-4">We are young students who are doing engeneering in the field of computer science so that we can deliver our ideas into the real world.So we come up with the idea of providing such platform for Nepal where we can find almost all info about Nepal.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+        <div class="container py-5">
+            <div class="row align-items-center mb-5">
+                <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+                    <h2 class="font-weight-light">Our Story</h2>
+                    <p class="font-italic text-muted mb-4">We are young students who are doing engeneering in the field of computer science so that we can deliver our ideas into the real world.So we come up with the idea of providing such platform for Nepal where we can find almost all info about Nepal.</p>
+                    <!-- <a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a> -->
+                </div>
+            
+                <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2">
+                    <img src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0">
+                </div>
+            </div>
+            <div class="row align-items-center mb-5">
+                <div class="col-lg-5 px-5 mx-auto">
+                    <img src="https://bootstrapious.com/i/snippets/sn-about/img-2.jpg" alt="" class="img-fluid mb-4 mb-lg-0">
+                </div>
+                <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
+                    <h2 class="font-weight-light">Our History</h2>
+                    <p class="font-italic text-muted mb-4">We came from different background to achieve same goal that is making impact to real world problem.We are Nepalese, our team contains only 3 members.</p>
+                    <!-- <a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a> -->
+                </div>
+            </div>
+            <div class="row align-items-center mb-5">
+                <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+                    <h2 class="font-weight-light">Our Mission</h2>
+                    <p class="font-italic text-muted mb-4">We are providing easy navigation,relaible,updated information to users of all kind who seeks the information about our country Nepal.</p>
+                    <!-- <a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a> -->
+                </div>
+            
+                <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2">
+                    <img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid">
+                </div>
+            </div>
         </div>
-        <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
-        </div>
-        <div class="row align-items-center">
-        <div class="col-lg-5 px-5 mx-auto"><img src="https://bootstrapious.com/i/snippets/sn-about/img-2.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
-        <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
-            <h2 class="font-weight-light">Our History</h2>
-            <p class="font-italic text-muted mb-4">We came from different background to achieve same goal that is making impact to real world problem.We are Nepalese, our team contains only 3 members.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
-        </div>
-        </div>
-    </div>
     </div>
 
     <div class="bg-light py-5">
