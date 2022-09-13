8d361b1b00be368a415f3ac1804686eafde5c1ee
sandesh-ghimire <ghimiresandesh45@gmail.com>
2022-09-13 20:02:42 +0530

    Features added (Admin login and dashboard).

---
+++ admin_blog_post/index.php
@@ -0,0 +1,65 @@
+<?php 
+
+//     session_start();
+//     include("includes/db.php");
+    
+//     if(!isset($_SESSION['admin_email'])){
+        
+//         echo "<script>window.open('login.php','_self')</script>";
+        
+//     }else{
+        
+//         $admin_session = $_SESSION['admin_email'];
+        
+//     }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Area</title>
+    <link rel="stylesheet" href="css/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div id="wrapper"><!-- #wrapper begin -->
+       
+       <?php include("includes/sidebar.php"); ?>
+       
+        <div id="page-wrapper"><!-- #page-wrapper begin -->
+            <div class="container-fluid"><!-- container-fluid begin -->
+                
+                <?php
+                
+                    if(isset($_GET['dashboard'])){
+                        
+                        include("dashboard.php");
+                        
+                }   if(isset($_GET['insert_blog'])){
+                        
+                        include("insert_blog.php");
+                        
+                }   if(isset($_GET['view_blog'])){
+                        
+                        include("view_blog.php");
+                        
+                 } 
+                
+                ?>
+                
+            </div><!-- container-fluid finish -->
+        </div><!-- #page-wrapper finish -->
+    </div><!-- wrapper finish -->
+
+<script src="js/jquery-331.min.js"></script>     
+<script src="js/bootstrap-337.min.js"></script>       
+</body>
+</html>
+
+
+<?php  ?>
