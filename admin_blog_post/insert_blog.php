8c481de16d16887692b1d867a99792c5d395ccf2
nirajgirixd <nirajgiri9860@gmail.com>
2022-10-01 10:57:06 +0530

    deleted admins table (new admintable table will be used), format of upload page has been updated

--- admin_blog_post/insert_blog.php
+++ admin_blog_post/insert_blog.php
@@ -1,182 +1,198 @@
 <?php 
 
-    if(!isset($_SESSION['admin_email'])){
-        
+    if (!isset($_SESSION['admin_email'])) {
         echo "<script>window.open('login.php','_self')</script>";
-        
-    }else{
+    }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Insert Blogs </title>
 </head>
+
 <body>
-    
-<div class="row"><!-- row Begin -->
-    
-    <div class="col-lg-12"><!-- col-lg-12 Begin -->
-        
-        <ol class="breadcrumb"><!-- breadcrumb Begin -->
-            
-            <li class="active"><!-- active Begin -->
-                
-                <i class="fa fa-dashboard"></i> Dashboard / Insert Blogs
-                
-            </li><!-- active Finish -->
-            
-        </ol><!-- breadcrumb Finish -->
-        
-    </div><!-- col-lg-12 Finish -->
-    
-</div><!-- row Finish -->
-       
-<div class="row"><!-- row Begin -->
-    
-    <div class="col-lg-12"><!-- col-lg-12 Begin -->
-        
-        <div class="panel panel-default"><!-- panel panel-default Begin -->
-            
-           <div class="panel-heading"><!-- panel-heading Begin -->
-               
-               <h3 class="panel-title"><!-- panel-title Begin -->
-                   
-                   <i class="fa fa-money fa-fw"></i> Insert Blog
-                   
-               </h3><!-- panel-title Finish -->
-               
-           </div> <!-- panel-heading Finish -->
-           
-           <div class="panel-body"><!-- panel-body Begin -->
-               
-               <form method="post" class="form-horizontal" enctype="multipart/form-data"><!-- form-horizontal Begin -->
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label"> Blog Title </label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <input name="Blog_title" type="text" class="form-control" required>
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-                  
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label"> Blog Category </label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <select name="Blog_cat" class="form-control"><!-- form-control Begin -->
-                              
-                              <option selected disabled> Select a Category </option>
-                              
-                             
-                              
-                          </select><!-- form-control Finish -->
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label">  Image 1 </label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <input name="b_img1" type="file" class="form-control" required>
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label">  Image 2 </label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <input name="b_img2" type="file" class="form-control">
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label">  Image 3 </label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <input name="b_img3" type="file" class="form-control form-height-custom">
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-                  
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label">  Keywords </label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <input name="keywords" type="text" class="form-control" required>
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label"> Blog Desc </label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <textarea name="Blog_desc" cols="19" rows="6" class="form-control"></textarea>
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-                   <div class="form-group"><!-- form-group Begin -->
-                       
-                      <label class="col-md-3 control-label"></label> 
-                      
-                      <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <input name="submit" value="Insert Blog" type="submit" class="btn btn-primary form-control">
-                          
-                      </div><!-- col-md-6 Finish -->
-                       
-                   </div><!-- form-group Finish -->
-                   
-               </form><!-- form-horizontal Finish -->
-               
-           </div><!-- panel-body Finish -->
-            
-        </div><!-- canel panel-default Finish -->
-        
-    </div><!-- col-lg-12 Finish -->
-    
-</div><!-- row Finish -->
-   
+
+    <!-- Breadcrumb Begin -->
+    <div class="row">
+        <div class="col-lg-12">
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i> Dashboard / Insert Blogs</li>
+            </ol>
+        </div>
+    </div>
+    <!-- Breadcrumb Finish -->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+
+                <!-- <div class="panel-heading">
+                    <h3 class="panel-title"><i class="fa fa-money fa-fw"></i> Insert Blog</h3>
+                </div> -->
+
+                <!-- Form Begins -->
+                <div class="panel-body">
+                    <form method="post" class="form-horizontal" enctype="multipart/form-data">
+
+                        <!-- Blog Title -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Blog Title</label>
+                            <div class="col-md-6">
+                                <input name="blog_title" type="text" class="form-control" placeholder="Title for the blog..." required>
+                            </div>
+
+                        </div>
+
+                        <!-- Arthur -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Arthur</label>
+                            <div class="col-md-6">
+                                <input name="arthur_name" type="text" class="form-control" placeholder="Your name..." required>
+                            </div>
+
+                        </div>
+
+                        <!-- Image 1 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Image 1</label>
+                            <div class="col-md-6">
+                                <input name="img1" type="file" accept="image/png, image/jpeg, image/jpg" class="form-control" required>
+                            </div>
+
+                        </div>
+
+                        <!-- Image 2 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Image 2</label>
+                            <div class="col-md-6">
+                                <input name="img2" type="file" accept="image/png, image/jpeg, image/jpg" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <!-- Image 3 -->
+                        <div class="form-group">
+                            
+                            <label class="col-md-3 control-label">Image 3</label>
+                            <div class="col-md-6">
+                                <input name="img3" type="file" accept="image/png, image/jpeg, image/jpg" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <!-- Image 4 -->
+                        <div class="form-group">
+                            
+                            <label class="col-md-3 control-label">Image 4</label>
+                            <div class="col-md-6">
+                                <input name="img4" type="file" accept="image/png, image/jpeg, image/jpg" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <!-- Image 5 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Image 5</label>
+                            <div class="col-md-6">
+                                <input name="img5" type="file" accept="image/png, image/jpeg, image/jpg" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <!-- Tag 1 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Tag 1</label>
+                            <div class="col-md-6">
+                                <input name="tag1" type="text" class="form-control" placeholder="#" required>
+                            </div>
+
+                        </div>
+
+                        <!-- Tag 2 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Tag 2</label>
+                            <div class="col-md-6">
+                                <input name="tag2" type="text" class="form-control" placeholder="#">
+                            </div>
+
+                        </div>
+
+                        <!-- Tag 3 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Tag 3</label>
+                            <div class="col-md-6">
+                                <input name="tag3" type="text" class="form-control" placeholder="#">
+                            </div>
+
+                        </div>
+
+                        <!-- Tag 4 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Tag 4</label>
+                            <div class="col-md-6">
+                                <input name="tag4" type="text" class="form-control" placeholder="#">
+                            </div>
+
+                        </div>
+
+                        <!-- Tag 5 -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label">Tag 5</label>
+                            <div class="col-md-6">
+                                <input name="tag5" type="text" class="form-control" placeholder="#">
+                            </div>
+
+                        </div>
+
+                        <!-- Blog Content -->
+                        <div class="form-group">
+                            
+                            <label class="col-md-3 control-label">Blog Content</label>
+                            <div class="col-md-6">
+                                <textarea name="Blog_desc" cols="19" rows="6" class="form-control"></textarea>
+                            </div>
+
+                        </div>
+                        
+                        <!-- Submit -->
+                        <div class="form-group">
+
+                            <label class="col-md-3 control-label"></label>
+                            <div class="col-md-6">
+                                <input name="submit" value="Insert Blog" type="submit" class="btn btn-primary form-control">
+                            </div>
+
+                        </div>
+
+                    </form>
+                    <!-- Form  Finish -->
+
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="js/tinymce/tinymce.min.js"></script>
-    <script>tinymce.init({ selector:'textarea'});</script>
+    <script>
+        tinymce.init({
+            selector: 'textarea'
+        });
+    </script>
 </body>
+
 </html>
-
-
-
-
-<?php } ?>
--- admin_blog_post/login.php
+++ admin_blog_post/login.php
@@ -3,28 +3,20 @@
     session_start();
     include("includes/db.php");
 
-    if(isset($_POST['admin_login'])){
+    if(isset($_POST['submit'])){
         
         $admin_email = mysqli_real_escape_string($con,$_POST['admin_email']);
-        
         $admin_pass = mysqli_real_escape_string($con,$_POST['admin_pass']);
         
-        $get_admin = "select * from admins where admin_email='$admin_email' AND admin_pass='$admin_pass'";
-        
-        $run_admin = mysqli_query($con,$get_admin);
-        
-        $count = mysqli_num_rows($run_admin);
+        $query = "select * from admintable where email='$admin_email' AND password='$admin_pass'";
+        $result = mysqli_query($con, $query);
+        $count = mysqli_num_rows($result);
         
         if($count==1){
-            
             $_SESSION['admin_email']=$admin_email;
-            
-            echo "<script>window.open('index.php?dashboard','_self')</script>";
-            
-        }else{
-            
+            echo "<script>window.open('index.php?dashboard','_self')</script>"; 
+        } else{
             echo "<script>alert('Email or Password is Wrong !')</script>";
-            
         }
     }
 ?>
@@ -34,30 +26,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>M-Dev Store Admin Area</title>
+    <title>Admin: Nepalese Stuff</title>
     <link rel="stylesheet" href="css/bootstrap-337.min.css">
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/login.css">
+
+    <!-- Favicon -->
+    <link rel="icon" href="../img/logo.png">
 </head>
 <body>
    
-   <div class="container"><!-- container begin -->
-       <form action="" class="form-login" method="post"><!-- form-login begin -->
-           <h2 class="form-login-heading"> Admin Login </h2>
+   <div class="container">
+
+        <form action="" class="form-login" method="post">
            
+            <h2 class="form-login-heading"> Admin Login </h2>
+            
            <input type="text" class="form-control" placeholder="Email Address" name="admin_email" required>
-           
            <input type="password" class="form-control" placeholder="Your Password" name="admin_pass" required>
            
-           <button type="submit" class="btn btn-lg btn-primary btn-block" name="admin_login"><!-- btn btn-lg btn-primary btn-block begin -->
-               
-               Login
-               
-           </button><!-- btn btn-lg btn-primary btn-block finish -->
-           
-       </form><!-- form-login finish -->
-   </div><!-- container finish -->
+           <button type="submit" class="btn btn-lg btn-primary btn-block" name="submit">Login</button>
+        
+       </form>
+
+   </div>
     
 </body>
 </html>
